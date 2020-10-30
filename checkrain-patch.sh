58302dd9400dacf37581363dc9d428f04928cdd0
Vitali Cohen <72298730+vivicoheng59@users.noreply.github.com>
2020-10-30 15:17:51 +0100

    Update checkrain-patch.sh

--- checkrain-patch.sh
+++ checkrain-patch.sh
@@ -4,7 +4,7 @@ PATCHED_FILE="checkra1n_patched"
 if [ $PLATFORM = "Linux" ] ; then
     curl - LO "https://assets.checkra.in/downloads/linux/cli/x86_64/fa08102ba978746ff38fc4c1a0d2e8f231c2cbf79c7ef6d7b504e4683a5b7d05/checkra1n"
     bspatch checkra1n $PATCHED_FILE checkra1n_A10_A11_linux.patch
-    chmod +x checkrain_patched
+    chmod +x checkra1n_patched
     echo "The patch has been applied."
     echo "You can run it like so: ./$PATCHED_FILE -c"
 
