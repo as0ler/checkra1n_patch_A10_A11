52379d6262bbf8ae4a51fcb479e79feb970d881b
Vitali Cohen <72298730+vivicoheng59@users.noreply.github.com>
2020-10-30 13:04:08 +0100

    added script for brainlets

---
+++ checkrain-patch.sh
@@ -0,0 +1,17 @@
+PLATFORM=$(uname)
+PATCHED_FILE="checkra1n_patched"
+
+if [ $PLATFORM = "Linux" ] ; then
+    curl - LO "https://assets.checkra.in/downloads/linux/cli/x86_64/fa08102ba978746ff38fc4c1a0d2e8f231c2cbf79c7ef6d7b504e4683a5b7d05/checkra1n"
+    bspatch checkra1n $PATCHED_FILE checkra1n_A10_A11_linux.patch
+    chmod +x checkrain_patched
+    echo "The patch has been applied."
+    echo "You can run it like so: ./$PATCHED_FILE -c"
+
+elif [ $PLATFORM = "Darwin" ] ; then
+    echo "Extracting the CLI tool from the checkra1n application."
+    cp /Applications/checkra1n.app/Contents/MacOS/checkra1n .
+    bspatch checkra1n $PATCHED_FILE checkra1n_A11_A10_mac.patch
+    echo "The patch has been applied."
+    echo "You can run it like so: ./$PATCHED_FILE -c"
+fi
